d9f96feaa8b7f514f448f4621f2b0f9dfe66fc47
Odane E Peck <odane.peck@mymona.uwi.edu>
2017-02-28 00:24:29 +0000

    created secure-page route

--- app/views.py
+++ app/views.py
@@ -54,6 +54,38 @@ def login():
         else:
                 flash('Username or Password is incorrect.', 'danger')# they should be redirected to a secure-page route instead
         return render_template("login.html", form=form)
+        
+form = LoginForm()
+    if request.method == "POST" and form.validate_on_submit():
+        return redirect(url_for('home'))
+        form = LoginForm()
+        # change this to actually validate the entire form submission
+        # and not just one field
+        if form.username.data:
+            # Get the username and password values from the form.
+            username = form.username.data
+            password = form.password.data
+            # using your model, query database for a user based on the username
+            # and password submitted
+            # store the result of that query to a `user` variable so it can be
+            # passed to the login_user() method
+            user = UserProfile.query.filter_by(username = username, password=password).first()
+        if user is not None:
+            # get user id, load into session
+            login_user(user)
+
+            # remember to flash a message to the user
+            flash("Log in successful", "success")
+            
+            return redirect(url_for("secure_page")) 
+        else:
+                flash('Username or Password is incorrect.', 'danger')# they should be redirected to a secure-page route instead
+        return render_template("login.html", form=form)
+        
+ @app.route("/secure-page")
+@login_required
+def secure_page():
+    return render_template('secure_page.html') 
 
 # user_loader callback. This callback is used to reload the user object from
 # the user ID stored in the session
